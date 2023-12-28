6880d80cb85e4525a5b8a93b62e792aa58f3c1ed
= <felipe.c.argolo@protonmail.com>
2023-12-28 12:29:55 -0300

    Dependencies + git diff

--- src/SymbolicInference.jl
+++ src/SymbolicInference.jl
@@ -1,8 +1,5 @@
 module SymbolicInference
 
-<<<<<<< HEAD
-#using DataStructures
-
 using StatsBase
 using RecurrenceAnalysis
 using AnalyticComb
@@ -11,10 +8,6 @@ using LinearAlgebra
 
 include("recurrence_plots.jl")
 
-
 export double_inference_weighted
-=======
-# Write your package code here.
->>>>>>> cc4410ab9b13fc90e3d8780f50a37ce949aa872b
 
 end
